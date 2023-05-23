5a50f64b291b4d81aa97e73f28f5171bbff3b2cf
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-05-24 00:15:56 +0530

    Create diag.ps1

---
+++ diag.ps1
@@ -0,0 +1,30 @@
+# Resource type for storage accounts
+$resourceType = "Microsoft.Storage/storageAccounts"
+
+# Get the list of storage accounts and their resource groups
+$storageAccounts = & az resource list --resource-type $resourceType --query "[].{ResourceGroup:resourceGroup, StorageAccount:name}" --output json | ConvertFrom-Json
+
+# Create an empty array to store the results
+$results = @()
+
+# Iterate over each storage account
+foreach ($account in $storageAccounts) {
+    # Get the diagnostic settings for the storage account
+    $diagnosticSettings = & az monitor diagnostic-settings list --resource $account.StorageAccount --resource-type $resourceType --resource-group $account.ResourceGroup --query "[].name" --output json | ConvertFrom-Json
+
+    # Get the diagnostic setting name or set it to "N/A" if no diagnostic setting is attached
+    $settingName = if ($diagnosticSettings) { $diagnosticSettings } else { "N/A" }
+
+    # Create a custom object to store the result
+    $result = [PSCustomObject]@{
+        "Storage Account" = $account.StorageAccount
+        "Resource Group" = $account.ResourceGroup
+        "Diagnostic Setting" = $settingName
+    }
+
+    # Add the result to the array
+    $results += $result
+}
+
+# Display the results in a table format
+$results | Format-Table -AutoSize
