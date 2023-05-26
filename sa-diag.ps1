3e7f6ec14ebe47bd75ffad1cfe01f95760a1ce5f
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-05-26 18:06:01 +0530

    Update sa-diag.ps1

--- sa-diag.ps1
+++ sa-diag.ps1
@@ -1,9 +1,8 @@
 # Resource type for storage accounts
 $resourceType = "Microsoft.Storage/storageAccounts"
-$eventHubResourceType = "Microsoft.EventHub/namespaces/eventhubs"
 
 # Subscription ID
-$subscriptionId = "xxxxxx-xxx-xxx-xxx-xxxxx"
+$subscriptionId = "xxxxxxx-xxx-xxx-xxx-xxxx"
 
 # Get the list of storage accounts and their resource groups
 $storageAccounts = & az resource list --resource-type $resourceType --subscription $subscriptionId --query "[].{ResourceGroup:resourceGroup, StorageAccount:name}" --output json | ConvertFrom-Json
@@ -19,22 +18,24 @@ foreach ($storageAccount in $storageAccounts) {
     # Get the diagnostic setting name or set it to "N/A" if no diagnostic setting is attached
     $settingName = if ($diagnosticSettings) { $diagnosticSettings } else { "N/A" }
 
-    # Initialize variable for Event Hub ID
-    $eventHubId = "N/A"
+    # Initialize variable for attached item
+    $attachedItem = "N/A"
 
-    # Get the attached details from diagnostic settings if a setting is attached
+    # Get the attached item from the diagnostic setting if a setting is attached
     if ($settingName -ne "N/A") {
         $diagnosticSetting = & az monitor diagnostic-settings show --name $settingName --resource $storageAccount.StorageAccount --resource-type $resourceType --resource-group $storageAccount.ResourceGroup --subscription $subscriptionId --output json | ConvertFrom-Json
 
-        if ($diagnosticSetting.eventHubAuthorizationRuleId) {
+        if ($diagnosticSetting.workspaceId) {
+            # Get the Log Analytics workspace ID
+            $logAnalyticsWorkspaceId = $diagnosticSetting.workspaceId
+
+            # Get the Log Analytics workspace name using the resource ID
+            $logAnalyticsWorkspace = & az resource show --ids $logAnalyticsWorkspaceId --subscription $subscriptionId --query "name" --output tsv
+            $attachedItem = "Log Analytics Workspace: $logAnalyticsWorkspace"
+        } elseif ($diagnosticSetting.eventHubAuthorizationRuleId) {
             # Extract the Event Hub namespace name from the authorization rule ID
             $eventHubNamespaceName = $diagnosticSetting.eventHubAuthorizationRuleId.Split("/")[-3]
-            
-            # Get the Event Hub resource ID using the namespace name and storage account name
-            $eventHubResourceId = & az resource show --resource-type $eventHubResourceType --name "$eventHubNamespaceName/$($storageAccount.StorageAccount)" --resource-group $storageAccount.ResourceGroup --subscription $subscriptionId --query "id" --output tsv
-
-            # Extract the Event Hub ID from the resource ID
-            $eventHubId = $eventHubResourceId.Split("/")[-1]
+            $attachedItem = "Event Hub Namespace: $eventHubNamespaceName"
         }
     }
 
@@ -43,61 +44,7 @@ foreach ($storageAccount in $storageAccounts) {
         "Storage Account" = $storageAccount.StorageAccount
         "Resource Group" = $storageAccount.ResourceGroup
         "Diagnostic Setting" = $settingName
-        "EventHubId" = $eventHubId
-    }
-
-    # Add the result to the array
-    $results += $result
-}
-
-# Display the results in a table format
-$results | Format-Table -AutoSize
-# Resource type for storage accounts
-$resourceType = "Microsoft.Storage/storageAccounts"
-$eventHubResourceType = "Microsoft.EventHub/namespaces/eventhubs"
-
-# Subscription ID
-$subscriptionId = "zzzzzz-zz-zzz-zzz-zzzzz"
-
-# Get the list of storage accounts and their resource groups
-$storageAccounts = & az resource list --resource-type $resourceType --subscription $subscriptionId --query "[].{ResourceGroup:resourceGroup, StorageAccount:name}" --output json | ConvertFrom-Json
-
-# Create an empty array to store the results
-$results = @()
-
-# Loop through each storage account
-foreach ($storageAccount in $storageAccounts) {
-    # Get the diagnostic settings for the storage account
-    $diagnosticSettings = & az monitor diagnostic-settings list --resource $storageAccount.StorageAccount --resource-type $resourceType --resource-group $storageAccount.ResourceGroup --subscription $subscriptionId --query "[].name" --output json | ConvertFrom-Json
-
-    # Get the diagnostic setting name or set it to "N/A" if no diagnostic setting is attached
-    $settingName = if ($diagnosticSettings) { $diagnosticSettings } else { "N/A" }
-
-    # Initialize variable for Event Hub ID
-    $eventHubId = "N/A"
-
-    # Get the attached details from diagnostic settings if a setting is attached
-    if ($settingName -ne "N/A") {
-        $diagnosticSetting = & az monitor diagnostic-settings show --name $settingName --resource $storageAccount.StorageAccount --resource-type $resourceType --resource-group $storageAccount.ResourceGroup --subscription $subscriptionId --output json | ConvertFrom-Json
-
-        if ($diagnosticSetting.eventHubAuthorizationRuleId) {
-            # Extract the Event Hub namespace name from the authorization rule ID
-            $eventHubNamespaceName = $diagnosticSetting.eventHubAuthorizationRuleId.Split("/")[-3]
-            
-            # Get the Event Hub resource ID using the namespace name and storage account name
-            $eventHubResourceId = & az resource show --resource-type $eventHubResourceType --name "$eventHubNamespaceName/$($storageAccount.StorageAccount)" --resource-group $storageAccount.ResourceGroup --subscription $subscriptionId --query "id" --output tsv
-
-            # Extract the Event Hub ID from the resource ID
-            $eventHubId = $eventHubResourceId.Split("/")[-1]
-        }
-    }
-
-    # Create a custom object to store the result
-    $result = [PSCustomObject]@{
-        "Storage Account" = $storageAccount.StorageAccount
-        "Resource Group" = $storageAccount.ResourceGroup
-        "Diagnostic Setting" = $settingName
-        "EventHubId" = $eventHubId
+        "Attached Item" = $attachedItem
     }
 
     # Add the result to the array
